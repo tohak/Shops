668e31bf57dd301f11fca61bfa44a0fa3bf98220
Антон <tohak199@gmail.com>
2020-07-16 23:16:08 +0300

    first

---
+++ test/src/main/java/com/shops/service/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.shops.service.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
